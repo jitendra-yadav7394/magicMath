31192e128f7c53c179abde6a04b475aa01a68b5a
unknown <Administrator@MC001MTO.wmd.local>
2019-10-08 12:10:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function isEvenNumber (number) {
+    return true ? number % 2 === 0 : false
+}
+
+module.exports = isEvenNumber;
---
+++ test.js
@@ -0,0 +1,4 @@
+var isEvenNumber = require('./index');
+
+console.log(isEvenNumber(5));
+console.log(isEvenNumber(4));
